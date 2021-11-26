071b2a71cc7fcd844d7bce7e1713544fa64525bf
Bilol Mamadjanov <bilol.mamadjanov.con@hotstar.com>
2021-11-26 15:07:13 +0500

    Add Xcode project

---
+++ WiFiLanChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WiFiLanChat
+//
+//  Created by Bilol Mamadjanov on 26/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
