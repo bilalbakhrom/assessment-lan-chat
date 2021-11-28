0a3b3fb9efab063c98b095275cdac2ebe3846116
Bilol Mamadjanov <bilol.mamadjanov.con@hotstar.com>
2021-11-28 16:55:29 +0500

    Update MessageInputView interface

--- WiFiLanChat/Views/MessageInputView.swift
+++ WiFiLanChat/Views/MessageInputView.swift
@@ -13,6 +13,7 @@ protocol MessageInputDelegate: AnyObject {
 
 class MessageInputView: UIView {
     weak var delegate: MessageInputDelegate?
+    private let uiConst = UIConstants()
     
     // MARK: - UI Properties
     private(set) lazy var textView: UITextView = {
@@ -20,26 +21,36 @@ class MessageInputView: UIView {
         view.layer.cornerRadius = 4
         view.layer.borderColor = UIColor(red: 200 / 255, green: 200 / 255, blue: 200 / 255, alpha: 0.6).cgColor
         view.layer.borderWidth = 1
+        view.font = .systemFont(ofSize: 16)
         
         return view
     }()
     
-    private(set) lazy var sendButton: UIButton = {
-        let view = UIButton()
-        view.backgroundColor = UIColor(red: 8 / 255, green: 183 / 255, blue: 231 / 255, alpha: 1.0)
-        view.layer.cornerRadius = 4
-        view.setTitle("Send", for: .normal)
+    private(set) lazy var sendButton: Button = {
+        let view = Button()
+        view.backgroundColor = .systemGreen
+        view.layer.cornerRadius = 8
+        view.set(title: "Send", color: .white)
         view.isEnabled = true
         view.addTarget(self, action: #selector(send), for: .touchUpInside)
         
         return view
     }()
     
+    private lazy var gradientLayer: CAGradientLayer = {
+        let layer = CAGradientLayer()
+        layer.colors = uiConst.gradientColors
+        layer.locations = uiConst.gradientLocations
+        layer.startPoint = uiConst.gradientStartPoint
+        layer.endPoint = uiConst.gradientEndPoint
+        
+        return layer
+    }()
+    
     // MARK: - Initializers
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
-        backgroundColor = UIColor(red: 247 / 255, green: 247 / 255, blue: 247 / 255, alpha: 1.0)
+                
         setup()
     }
     
@@ -56,6 +67,7 @@ class MessageInputView: UIView {
     
     override func layoutSubviews() {
         super.layoutSubviews()
+        gradientLayer.frame = bounds
         
         let size = bounds.size
         textView.bounds = CGRect(x: 0, y: 0, width: size.width - 32 - 8 - 60, height: 40)
@@ -74,11 +86,27 @@ extension MessageInputView: UITextViewDelegate {
 
 extension MessageInputView {
     func setup() {
+        backgroundColor = UIColor(red: 247 / 255, green: 247 / 255, blue: 247 / 255, alpha: 1.0)
+        layer.borderWidth = 1.0
+        layer.borderColor = UIColor.systemGray4.cgColor
         embedSubviews()
     }
     
     private func embedSubviews() {
         addSubview(textView)
         addSubview(sendButton)
+        layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
+
+extension MessageInputView {
+    private struct UIConstants {
+        let gradientColors: [CGColor] = [
+            UIColor(red: 0.882, green: 0.902, blue: 0.941, alpha: 1).cgColor,
+            UIColor(red: 0.69, green: 0.729, blue: 0.82, alpha: 1).cgColor
+        ]
+        let gradientLocations: [NSNumber] = [0, 1]
+        let gradientStartPoint = CGPoint(x: 0.25, y: 0.5)
+        let gradientEndPoint = CGPoint(x: 0.75, y: 0.5)
     }
 }
